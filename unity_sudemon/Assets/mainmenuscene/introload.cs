e1995a5ba34d5d56cb6df2fd93fc6126e56eefbc
user <hello3181@gmail.com>
2021-05-09 21:54:19 +0900

    Start/Quit button
    mainmenu 폴더 안에

---
+++ unity_sudemon/Assets/mainmenuscene/introload.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class introload : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void GameStart()
+    {
+        SceneManager.LoadScene("intro");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
