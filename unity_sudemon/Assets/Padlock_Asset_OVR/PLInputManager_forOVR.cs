969cab5a926883d00e4c11b0be37df99bb9b3d13
Julse77 <wrs0707@gmail.com>
2021-05-17 02:06:17 +0900

    2021-05-17
    1스테이지
    1. 로컬 아바타 하위로 그랩 프리펩 추가
    2. 신문 리지드바디, 박스콜라이더 적용
    
    테스트 씬
    1. 패드락 카메라 메커니즘 온 까지 가능

---
+++ unity_sudemon/Assets/Padlock_Asset_OVR/PLInputManager_forOVR.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+
+namespace PadlockSystem
+{
+    public class PLInputManager_forOVR : MonoBehaviour
+    {
+        [Header("Raycast Pickup Input")]
+        public OVRInput.Button interactKey;
+        public OVRInput.Button closeKey;
+
+        [Header("Trigger Inputs")]
+        public OVRInput.Button triggerInteractKey;
+
+        public static PLInputManager_forOVR instance;
+
+        private void Awake()
+        {
+            if (instance != null) { Destroy(gameObject); }
+            else { instance = this; DontDestroyOnLoad(gameObject); }
+        }
+    }
+}
