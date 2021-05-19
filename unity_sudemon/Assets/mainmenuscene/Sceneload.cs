c9e4e45e6954d7c3cabf408ce20cb47d6c563c91
user <hello3181@gmail.com>
2021-05-19 21:59:05 +0900

    2021-05-19 21:58
    인트로
    -Scenes 폴더 안 intro씬 추가
       ->영상(수정전) 추가, 넘어가기 버튼추가
    -meinmenuscene 폴더 안 Sceneload 스크립트 추가
       ->넘어가기 버튼 누르면 mainmenu로 이동
    
    Main menu
    -버튼 동작 구현
       ->시작 버튼 : 1stage로 이동
       ->종료 버튼 : 게임 종료
    -시작하기 버튼 이미지 사이즈 변경
    
    Build Settings
    -Scenes In Build에 intro 추가, mainmenu 체크
    
    [메인 메뉴 및 인트로 씬에 사용된 모든 이미지는 폴더mainmenuscene에 존재]

---
+++ unity_sudemon/Assets/mainmenuscene/Sceneload.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Sceneload : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void GOmain()
+    {
+        SceneManager.LoadScene("mainmenu");
+    }
+
+    public void Gamestart()
+    {
+        SceneManager.LoadScene("1st_stage_Scene");
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+    
+}
