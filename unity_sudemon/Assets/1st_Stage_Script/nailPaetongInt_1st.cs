0bd643d72d0fd1ee8dc1135936054264b6a5f8fe
Julse77 <wrs0707@gmail.com>
2021-07-10 19:00:04 +0900

    2021-07-10 19:00
    1st Stage
    못과 패통버튼 상호작용 스크립트 추가
    (nailPaetongInt_1st.cs)

---
+++ unity_sudemon/Assets/1st_Stage_Script/nailPaetongInt_1st.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class nailPaetongInt_1st : MonoBehaviour
+{
+    // Collider 충돌 시 이벤트 발생
+    private void OnCollisionEnter(Collision collision)
+    {
+        // paetongButton 태그의 gameObject와 충돌 시
+        if (collision.gameObject.tag == "paetongButton")
+        {
+            OpenPaetong();
+        }        
+    }
+
+    void OpenPaetong()
+    {
+        // paetong_in 이름의 GameObject의 컴포넌트 conPaetong_1st의 PlayAnimation 함수 실행
+        GameObject.Find("paetong_in").GetComponent<conPaetong_1st>().PlayAnimation();
+    }
+
+}
