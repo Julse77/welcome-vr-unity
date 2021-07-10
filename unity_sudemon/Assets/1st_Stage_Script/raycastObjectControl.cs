c208ea0ef79dc66c7fec373846e91bca8d440fa1
Julse77 <wrs0707@gmail.com>
2021-07-10 16:47:05 +0900

    2021-07-10 16:46
    1st stage
    - raycastObjectControl.cs 추가.
      - 하위로 두번째 방 창문을 여는 스크립트가 있다.

---
+++ unity_sudemon/Assets/1st_Stage_Script/raycastObjectControl.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class raycastObjectControl : MonoBehaviour
+{
+    private RaycastHit hit;
+    private float RayDistance = 30f;
+
+    private windowOpen_1st PlayAnimation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 좌, 우측 컨트톨러 검지
+        if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger) || OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out hit, RayDistance))
+            {
+                // ray가 tag에 닿았을 때, 애니메이션 On
+                if (hit.collider.tag == "windowOpen_1st")
+                {
+                    GameObject.Find("rightwindow").GetComponent<windowOpen_1st>().PlayAnimation();
+                }
+            }
+        }
+    }
+}
