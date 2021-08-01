4b1b3fd0b6a55de578e5fdd66e19bb49e0415598
민지수 <wrs0707@gmail.com>
2021-08-01 15:16:22 +0900

    2021-08-01 15:14
    1 stage
    1. raycastObjectControl.cs
    - tagName 변수 추가 및 주석 추가
    2. korean mail_(1) -> Materials 하위로 이동
    3. no static 하위 room_Object 생성

--- unity_sudemon/Assets/1st_Stage_Script/raycastObjectControl.cs
+++ unity_sudemon/Assets/1st_Stage_Script/raycastObjectControl.cs
@@ -4,22 +4,26 @@ using UnityEngine;
 
 public class raycastObjectControl : MonoBehaviour
 {
+    // 변수선언
     private RaycastHit hit;
-    public float RayDistance = 30f;
+    public float RayDistance = 50f;
+    private string tagName = "windowOpen_1st";
 
     private windowOpen_1st PlayAnimation;
 
     // Update is called once per frame
     void Update()
     {
-        // 좌, 우측 컨트톨러 검지
+        // 컨트롤러 양손 검지파트를 누를 시
         if (OVRInput.Get(OVRInput.Button.SecondaryIndexTrigger) || OVRInput.Get(OVRInput.Button.PrimaryIndexTrigger))
         {
+            // ray 발사
             if (Physics.Raycast(transform.position, transform.forward, out hit, RayDistance))
             {
                 // ray가 tag에 닿았을 때, 애니메이션 On
-                if (hit.collider.tag == "windowOpen_1st")
+                if (hit.collider.CompareTag(tagName))
                 {
+                    Debug.Log("창문 열림!");
                     GameObject.Find("rightwindow").GetComponent<windowOpen_1st>().PlayAnimation();
                 }
             }
