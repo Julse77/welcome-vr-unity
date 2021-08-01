46adc1ac26e802248827e07d97ef251a9b0952e9
민지수 <wrs0707@gmail.com>
2021-08-01 16:40:20 +0900

    2021-08-01 16:40
    1st_Locked_Box1
    - animation 생성(1st_openBox1)
    - script 생성(openBox_1st)

---
+++ unity_sudemon/Assets/1st_Stage_Script/openBox_1st.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class openBox_1st : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+    public void PlayAnimation()
+    {
+        if (null != anim)
+        {
+            anim.Play("1st_openBox1", 0, 0.0f);
+        }
+    }
+}
