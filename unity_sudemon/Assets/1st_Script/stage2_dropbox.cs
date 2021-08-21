8afb534f63834c0bcd4f30cab0e7cb8ea27db311
user <hello3181@gmail.com>
2021-08-21 21:09:13 +0900

    2021-08-21 21:02
    2stage
    -OVRPlayerController-RightHandAnchor에 OVR Grabber 스크립트 추가 및 설정
    -2st_rope 1,2에 rigidbody(x,y,z 모두 Freeze position, Freeze Rotation상태), box Collider, OVR Grabbable스크립트 추가
    -손톱 추가 생성 및 위치 조절, 2st_roxbox 위치 조절, 오브젝트에 rigidbody 및 collider 적용
    -rope1에는 stage2_rope 스크립트 적용(사용자의 손이 닿으면 손톱 활성화되며 바닥으로 떨어짐)
    -rope2에는 stage2_dropbox 스크립트 적용(사용자의 손이 닿으면 박스가 떨어짐)
    -OVRPlayerController-RightHandAnchor는 PlayerHand 태그로 설정

---
+++ unity_sudemon/Assets/1st_Script/stage2_dropbox.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stage2_dropbox : MonoBehaviour
+{
+    public GameObject box;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "PlayerHand")
+        {
+
+            box.SetActive(true);
+        }
+
+    }
+}
---
+++ unity_sudemon/Assets/1st_Script/stage2_rope.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stage2_rope : MonoBehaviour
+{
+    public GameObject nails;
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "PlayerHand")
+        {
+
+            nails.SetActive(true);
+        }
+
+    }
+}
