306174cefc4a84b75f663efd561db902c0170186
user <hello3181@gmail.com>
2021-08-29 22:24:20 +0900

    2021-08-29 22:24
    1Stage
    -Rusty_Nail 1 에 Nail 태그 붙임
    -1st_Script - paetongdrop 스크립트 작성(패통 떨어지는 소리, 패통 애니메이션 재생)
    -1st_paetongbutton_drop 오브젝트 생성해 Rusty Nail 1과 충돌 시킴. 하위에 paetongSound 생성.

---
+++ unity_sudemon/Assets/1st_Script/paetongdrop.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class paetongdrop : MonoBehaviour
+{
+
+    private Animator anim;
+    public GameObject paetong_in;
+    public GameObject paetongAudio;
+    
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Nail")
+        {
+            paetongAudio.SetActive(true);
+            anim = this.paetong_in.GetComponent<Animator>();
+            anim.Play("1st_contPaetong");
+            
+        }
+
+    }
+    
+}
