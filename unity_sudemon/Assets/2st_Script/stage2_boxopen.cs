38ac83670a54bba7e578814428340311c7a9251b
user <hello3181@gmail.com>
2021-09-05 21:12:22 +0900

    2021-09-05 21:08
    2stage
    -key 오브젝트에 태그 붙임
    -key에 rigidbody, distanceGrabbable 스크립트, 박스콜라이더추가
    -2st_roxbox(1)에 stage2_boxopen 스크립트 추가(상자에 열쇠가 닿으면 상자가 열리는 애니메이션이 실행 되도록 하는 스크립트)->애니메이션은 아직

---
+++ unity_sudemon/Assets/2st_Script/stage2_boxopen.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stage2_boxopen : MonoBehaviour
+{
+    private Animator anim;
+
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "Key")
+        {
+            //anim = this.paetong_in.GetComponent<Animator>();
+            //anim.Play("1st_contPaetong");
+            //키와 닿으면 상자열리는 애니메이션
+        }
+
+    }
+}
