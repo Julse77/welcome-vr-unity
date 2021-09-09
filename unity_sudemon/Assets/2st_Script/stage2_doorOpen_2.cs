685a1a136542da85a735d012ed1aa1998c6b68c8
Julse77 <wrs0707@gmail.com>
2021-09-09 23:16:49 +0900

    2021-09-09 23:15
    2stage
    - 두번째 및 세번째 방 문 스크립트 추가
    - 각 문에 Padlock 추가 및 애니메이션 연결

---
+++ unity_sudemon/Assets/2st_Script/stage2_doorOpen_2.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class stage2_doorOpen_2 : MonoBehaviour
+{
+    private Animator anim;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    public void PlayAnimation()
+    {
+        if (null != anim)
+        {
+            anim.Play("2st_openSecondDoor", 0, 0.0f);
+        }
+    }
+}
