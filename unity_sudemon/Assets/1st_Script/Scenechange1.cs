4bbf3ca45d1d23f846e90f208798e9388c7c2916
user <hello3181@gmail.com>
2021-09-19 21:25:22 +0900

    2021-09-19 21:19
    1stage
    -2stage로 이동하는 스크립트 추가(Scenechange1)
    :오브젝트를 집으면 명부 획득 자막이 생성되고 5초 후 다음 스테이지로 이동. 자막 위치 조절 필요함.)
    -OVRPlayerController 하위에 Canvas(명부획득이미지)추가함.
    -subtitles 폴더 추가(일단 자막을 이미지로 만들었음)
    -테스트한 오브젝트(SceneChangeTest1) 비활성화 시켜둠.
    
    2stage
    -건곤감리 카드 레이어를 Interact로 변경

---
+++ unity_sudemon/Assets/1st_Script/Scenechange1.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Scenechange1 : MonoBehaviour
+{
+    public GameObject image1;
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.tag == "PlayerHand")
+        {
+            image1.SetActive(true);
+            Invoke("Scene2", 5f);
+        }
+
+    }
+    void Scene2()
+    {
+        SceneManager.LoadScene("2st_Scene");
+    }
+}
