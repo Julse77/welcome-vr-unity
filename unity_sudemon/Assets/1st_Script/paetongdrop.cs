1a84cdf57604d52b5a226234a2b2f570998a3187
user <hello3181@gmail.com>
2021-11-02 23:21:20 +0900

    2021-11-02 23:20
    1스테이지
    -1st_Scene_4C 마지막 철장문 통과 가능하게 설정됨
    -1st_Scene_2C 못 사용해서 패통 떨어트리기 설정됨(이 때 또 멈춤)
    
    2스테이지
    -명부가 잘 안집히는 것 같아 가운데로 위치 옮겨둠(잘 안보여서 작게 조명 설치)
    -물고문실 상자의 위치를 옮겨둠
    -물고문실 그림을 담는 통 크기를 살짝 키움
    
    3스테이지
    -3st_Scene_2C 레버 그랩 가능하게 변경됨
    
    ※떨어지는 속도가 빠를시 충돌이 무시되고 바닥으로 추락하는 듯 해 일단 손으로 집는 오브젝트들의 추락 속도를 느리게 만들어둠.
    
    *본인 플레이시 그림을 주웠을 때에는 멈추지 않았으나 물고문실 자물쇠 열었을 때 동작이 멈춤. 서신을 건넬 때에도 동작이 멈춤.
    *2스테이지 첫번째 방에서 두번째방으로 넘어갈 때 자물쇠가 작동이 안 됨.
    *몇몇 설정들이 간혹 오류인지 풀려있음을 확인. 몇 개의 설정을 다시 만짐.

--- unity_sudemon/Assets/1st_Script/paetongdrop.cs
+++ unity_sudemon/Assets/1st_Script/paetongdrop.cs
@@ -8,11 +8,18 @@ public class paetongdrop : MonoBehaviour
     private Animator anim;
     public GameObject paetong_in;
     public GameObject paetongAudio;
+    public GameObject nailanim;
+    private Animator anim2;
+    public GameObject nail2;
     
     private void OnTriggerStay(Collider other)
     {
         if (other.tag == "Nail")
         {
+            nail2.SetActive(false);
+            nailanim.SetActive(true);
+            anim2 = this.nailanim.GetComponent<Animator>();
+            anim2.Play("1st_Nail");
             paetongAudio.SetActive(true);
             anim = this.paetong_in.GetComponent<Animator>();
             anim.Play("1st_contPaetong");
