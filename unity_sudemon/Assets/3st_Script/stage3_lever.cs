1f13afced6d59059bbf398a8d711272d2b47317f
user <hello3181@gmail.com>
2021-11-05 00:59:17 +0900

    2021-11-05 00:58
    3stage
    -씬추가 3st_Scene_1C_1
    -3st_Scene_1C에서 레버를 놓으면 3st_Scene_1C_1으로 이동됨

--- unity_sudemon/Assets/3st_Script/stage3_lever.cs
+++ unity_sudemon/Assets/3st_Script/stage3_lever.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class stage3_lever : MonoBehaviour
 {
@@ -18,8 +19,12 @@ public class stage3_lever : MonoBehaviour
             lever2.SetActive(false);
             anim = this.lever1.GetComponent<Animator>();
             anim.Play("3st_lever");
+            Invoke("Scene3", 2f);
         }
 
     }
-
+    void Scene3()
+    {
+        SceneManager.LoadScene("3st_Scene_1C_1");
+    }
 }
