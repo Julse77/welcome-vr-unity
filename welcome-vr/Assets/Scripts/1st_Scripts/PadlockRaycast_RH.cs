daa38add14cde17e8beaaa86b2ee5dc1bd140088
Julse77 <79743119+Julse77@users.noreply.github.com>
2024-01-26 14:46:16 +0900

    Fixed bug about Padlock control
    Fixed a bug where 'row' for all locks would return.
    - Edit PadlockRaycast_RH.cs
    - Add Tags

--- welcome-vr/Assets/Scripts/1st_Scripts/PadlockRaycast_RH.cs
+++ welcome-vr/Assets/Scripts/1st_Scripts/PadlockRaycast_RH.cs
@@ -3,49 +3,116 @@ using System.Collections.Generic;
 using UnityEngine;
 
 using PadlockSystem;
+using System.Runtime.CompilerServices;
 
 public class PadlockRaycast_RH : MonoBehaviour
 {
     // Padlock_Camera_Mechanism 하위 Padlock_Combination
-    public SpinnerScript row1, row2, row3, row4 = new SpinnerScript();
+    [Header("First Padlock's Rows")]
+    public SpinnerScript first_Row1, first_row2, first_Row3, first_Row4 = new SpinnerScript();
+    [Header("Second Padlock's Rows")]
+    public SpinnerScript second_Row1, second_Row2, second_Row3, second_Row4 = new SpinnerScript();
+    [Header("Third Padlock's Rows")]
+    public SpinnerScript third_Row1, third_Row2, third_Row3, third_Row4 = new SpinnerScript();
+    [Header("Last Padlock's Rows")]
+    public SpinnerScript last_Row1, last_Row2, last_Row3, last_Row4 = new SpinnerScript();
 
     RaycastHit hit;
-    public float RayDistance = 15f;
+    private float RayDistance = 15f;
 
     void Update()
     {
         // 우측 컨트롤러 스틱
         if (OVRInput.GetDown(OVRInput.Button.One))
-        {            
-            if(Physics.Raycast(transform.position, transform.forward, out hit, RayDistance))
-            {
-                // 맞는 태그에 Ray가 닿으면 돈다
-                if (hit.collider.tag == "Row1")
-                {
-                    row1.Spin();
-                }
-
-                else if (hit.collider.tag == "Row2")
-                {
-                    row2.Spin();
-                }
-
-                else if (hit.collider.tag == "Row3")
-                {
-                    row3.Spin();
-                }
-
-                else if (hit.collider.tag == "Row4")
-                {
-                    row4.Spin();
-                }
-
-                else{
-
-                }
-
-            }
+        {
+            RaySpinFunc();
         }
     }
 
+    private void RaySpinFunc()
+    {
+        if (Physics.Raycast(transform.position, transform.forward, out hit, RayDistance))
+        {
+            // 맞는 태그에 Ray가 닿으면 돈다
+            if (hit.collider.tag == "FirstRow1")
+            {
+                first_Row1.Spin();
+            }
+
+            if (hit.collider.tag == "FirstRow2")
+            {
+                first_row2.Spin();
+            }
+
+            if (hit.collider.tag == "FirstRow3")
+            {
+                first_Row3.Spin();
+            }
+
+            if (hit.collider.tag == "FirstRow4")
+            {
+                first_Row4.Spin();
+            }
+
+            if (hit.collider.tag == "SecondRow1")
+            {
+                second_Row1.Spin();
+            }
+
+            if (hit.collider.tag == "SecondRow2")
+            {
+                second_Row2.Spin();
+            }
+
+            if (hit.collider.tag == "SecondRow3")
+            {
+                second_Row3.Spin();
+            }
+
+            if (hit.collider.tag == "SecondRow4")
+            {
+                second_Row4.Spin();
+            }
+
+            if (hit.collider.tag == "ThirdRow1")
+            {
+                third_Row1.Spin();
+            }
+
+            if (hit.collider.tag == "ThirdRow2")
+            {
+                third_Row2.Spin();
+            }
+
+            if (hit.collider.tag == "ThirdRow3")
+            {
+                third_Row3.Spin();
+            }
+
+            if (hit.collider.tag == "ThirdRow4")
+            {
+                third_Row4.Spin();
+            }
+
+            if (hit.collider.tag == "LastRow1")
+            {
+                last_Row1.Spin();
+            }
+
+            if (hit.collider.tag == "LastRow2")
+            {
+                last_Row2.Spin();
+            }
+
+            if (hit.collider.tag == "LastRow3")
+            {
+                last_Row3.Spin();
+            }
+
+            if (hit.collider.tag == "LastRow4")
+            {
+                last_Row4.Spin();
+            }
+        }
+    }
 }
