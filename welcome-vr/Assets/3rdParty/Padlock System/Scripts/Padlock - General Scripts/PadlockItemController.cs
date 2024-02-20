da6fba17f71ffe879152739f66384f1c2ff8346a
Julse77 <79743119+Julse77@users.noreply.github.com>
2024-02-21 02:33:47 +0900

    Edit gitignore
    and remove bgm folder

---
+++ welcome-vr/Assets/3rdParty/Padlock System/Scripts/Padlock - General Scripts/PadlockItemController.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace PadlockSystem
+{
+    public class PadlockItemController : MonoBehaviour
+    {
+        [SerializeField] private PadlockController _padlockController;
+
+        public void ShowKeypad()
+        {
+            _padlockController.ShowPadlock();
+            Debug.Log($"### This padlock Controller is {_padlockController}");
+        }
+    }
+}
